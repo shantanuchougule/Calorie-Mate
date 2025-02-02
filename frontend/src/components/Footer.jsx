3b83d2f31b2fd286151bcbe741cd32ffae2fe1a2
Shantanu <shantanuchougule7595@gmail.com>
2025-02-03 01:08:19 +0530

    Fix Vite build issue

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer-custom text-center text-white bg-primary">
-        <h2 class="developer text-center"><a className="text-light" href="https://shantanuchougule.vercel.app/">Developed by Shantanu Chougule</a></h2>
+        <h2 className="developer text-center"><a className="text-light" href="https://shantanuchougule.vercel.app/">Developed by Shantanu Chougule</a></h2>
       <p>© {new Date().getFullYear()} Shantanu Chougule | All Rights Reserved</p>
     </footer>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: './', // Ensure Vite looks in the right place
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
 });
