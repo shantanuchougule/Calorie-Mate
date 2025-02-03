7fa8234145712fee07788aa4b065866e9794d80e
Shantanu <shantanuchougule7595@gmail.com>
2025-02-03 10:33:56 +0530

    Fixed Vercel deployment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // Ensure Vite is outputting the build files correctly
-  }
+    outDir: "dist" // Ensure Vite is outputting to `dist`
+  },
+  base: "./" // Ensures relative paths for assets
 });
